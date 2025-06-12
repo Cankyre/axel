fb8caa5fa78811d1d9921cc54ad5654ec4923b05
Cankyre <me@cankyre.eu>
2025-06-12 15:40:11 +0200

    Negamax function

--- src/Search/Negamax.jl
+++ src/Search/Negamax.jl
@@ -1,1 +1,40 @@
+using Chess
 
+function negamax(board::Board, depth::Int)
+    return negamax(board, depth, -Inf, Inf)
+end
+
+function negamax(
+    board::Board,
+    depth::Int,
+    α::Float64,
+    β::Float64,
+)
+    if depth == 0 || is_search_cancelled()
+        # TODO Replace with actual evaluation function
+        return ([], rand(-10.0:10.0:0.1, 1)[1])  
+    end
+
+    best_value = -Inf
+    best_pv = []
+    movelist = moves(board)
+
+    for move in movelist
+        undoinfo = domove!(board, move)
+        (pv, value) = negamax(board, depth - 1, -β, -α)
+        undomove!(board, undoinfo)
+        value = -value
+
+        if value > best_value
+            best_value = value
+            best_pv = [move, pv]
+        end
+
+        α = max(α, value)
+        if α >= β
+            break
+        end
+    end
+
+    return (best_pv, best_value)
+end
